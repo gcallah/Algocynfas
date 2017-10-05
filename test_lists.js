966d2bfcc714e51336aa92bc97fe421a6f6b0ee0
Vainika <vainika.dara@gmail.com>
2017-10-05 11:36:59 -0400

    Changing the color of an element

--- test_lists.js
+++ test_lists.js
@@ -9,8 +9,7 @@ var height=parseInt(document.getElementById("canvas").getAttribute("height"));
 var width=parseInt(document.getElementById("canvas").getAttribute("width"));
 var position = width/2;
 
-function addRowElem(value, next_to = null, place = RIGHT)
-{
+function addRowElem(value, next_to = null, place = RIGHT) {
     /* If first elem, place in center. If not, we need LEFT or RIGHT. */
     // create a rectangle object
     var rect = new fabric.Rect({
@@ -18,18 +17,22 @@ function addRowElem(value, next_to = null, place = RIGHT)
       originY: 'center',
       fill: 'red',
       width: 20,
-      height: 20
+      height: 20,
+      id: value,
     });
     var strValue=String(value);
     var text= new fabric.Text(strValue, {fontSize: 10, originX: 'center', originY: 'center'});
-    var group = new fabric.Group([ rect, text ], {left:position, top: 40, angle:0});
+    var group = new fabric.Group([ rect, text ], {left:position, top: 40, angle:0,});
     canvas.add(group);
     position += 21;
     return group;
 }
 
-function colorElem(value)
-{
+function colorElem(value) {
+  canvas.getObjects().map((item) =>
+  item.getObjects().map((node) =>
+  node.id === value ? node.set('fill','pink'): ''
+  ));
 }
 
 function createList()
