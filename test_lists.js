b98cd7ff297a8f4cc6db1e04cd60c3f33c9d7cdd
gcallah <gcallah@mac.com>
2017-10-05 16:40:55 -0400

    Code review.

--- test_lists.js
+++ test_lists.js
@@ -9,49 +9,49 @@ var width=parseInt(document.getElementById("canvas").getAttribute("width"));
 var position = width/2;
 var arr = [];
 
-function addCanElem(value) {
-  var rect = new fabric.Rect({
-    originX: 'center',
-    originY: 'center',
-    fill: 'red',
-    width: 20,
-    height: 20,
-    id: value,
-  });
-  var strValue=String(value);
-  var text= new fabric.Text(strValue, {fontSize: 10, originX: 'center', originY: 'center'});
-  var group = new fabric.Group([ rect, text ], {left:position, top: 40, angle:0, id:value});
-  canvas.add(group);
-  position += 21;
+function addCanvasElem(value) {
+    var rect = new fabric.Rect({
+        originX: 'center',
+        originY: 'center',
+        fill: 'red',
+        width: 20,
+        height: 20,
+        id: value,
+    });
+    var strValue=String(value);
+    var text= new fabric.Text(strValue, {fontSize: 10, originX: 'center', originY: 'center'});
+    var group = new fabric.Group([ rect, text ], {left: position, top: 40, angle: 0, id: value});
+    canvas.add(group);
+    position += 21;
 }
 
 function addRowElem(value, next_to = null, place = RIGHT) {
     /* If first elem, place in center. If not, we need LEFT or RIGHT. */
     // create a rectangle object
-    createList(value,next_to,place);
+    resetCanvas();
+    redrawList(value, next_to, place);
     arr.map((item) =>
-      addCanElem(item)
+      addCanvasElem(item)
     );
 }
 
 function colorElem(value) {
-  canvas.getObjects().map((item) =>
-  item.getObjects().map((node) =>
-  node.id === value ? node.set('fill','pink'): ''
-  ));
+    canvas.getObjects().map((item) =>
+    item.getObjects().map((node) =>
+    node.id === value ? node.set('fill', 'pink'): ''
+    ));
 }
 
-function removeElem() {
-  canvas.clear();
-  position = width/2;
+function resetCanvas() {
+    canvas.clear();
+    position = width/2;
 }
 
-function createList(value, next_to, place) {
-  removeElem();
-  if(next_to) {
-  var eleIndex = arr.indexOf(next_to);
-  place === 'LEFT' ? arr.splice(eleIndex, 0, value) : arr.splice(eleIndex + 1, 0, value)
-  } else {
-   arr.push(value);
-  }
+function redrawList(value, next_to, place) {
+    if(next_to) {
+        var eleIndex = arr.indexOf(next_to);
+        place === 'LEFT' ? arr.splice(eleIndex, 0, value) : arr.splice(eleIndex + 1, 0, value)
+    } else {
+        arr.push(value);
+    }
 }
