5dd31a87819158631b3b58f7abaa67606be5aed5
gcallah <gcallah@mac.com>
2017-10-20 16:00:50 -0400

    Experimenting with delay.

--- insertion_sort.js
+++ insertion_sort.js
@@ -5,8 +5,13 @@ async function insertionSort(elements) {
         var key = elements[j];
         var i = j -1;
         highlightKey(key);
+        resetCanvas();
+        displayList(elements);
         while(i >= 0 && elements[i] > key) {
             highlight(elements[i]);
+            await delay(DEFAULT_DELAY);
+            resetCanvas();
+            displayList(elements);
             elements[i + 1] = elements[i];
             i = i - 1
         }
