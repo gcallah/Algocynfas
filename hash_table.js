fa6d4982b31bd23d2b4db04b6e0a970f8c02aed0
Vainika <vainika.dara@gmail.com>
2017-11-09 14:41:11 -0500

    Added Intial Hash Table

---
+++ hash_table.js
@@ -0,0 +1,25 @@
+function hashTable(ts) {
+    this.values = Array(5);
+    for(i = 0; i < ts; i++) {
+        this.values[i] = i;
+    }
+    this.numberOfValues = 0;
+    this.ts = ts;
+}
+
+hashTable.prototype.insert = function(value) {
+    var hindex  = calDivHash(value, this.ts);
+    if(this.values[hindex] === hindex) {
+       this.values[hindex] = [];
+    }
+    this.numberOfValues++;
+    this.values[hindex].push(hindex, value);
+};
+
+function calDivHash(k, ts) {
+    return k % ts;
+}
+
+function displayHT(table){
+  displayHashTable(table);
+}
