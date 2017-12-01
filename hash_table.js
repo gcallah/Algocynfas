8eb9ffb480a5de0d9b1542a68b79d52d48e7b7dd
Vainika <vainika.dara@gmail.com>
2017-12-01 12:12:58 -0500

    Minor Changes

--- hash_table.js
+++ hash_table.js
@@ -3,23 +3,18 @@ function hashTable(table_size) {
     for(i = 0; i < table_size; i++) {
         this.values[i] = [];
     }
-    this.numberOfValues = 0;
     this.table_size = table_size;
 }
 
 hashTable.prototype.insert = function(value) {
     var hindex  = calDivHash(value, this.table_size);
-    this.numberOfValues++;
     this.values[hindex].push(value);
-    this.values[hindex].length === 0 ?
-        this.values[hindex].push(hindex, value) :
-        this.values[hindex].push(value);
 };
 
-function calDivHash(k, table_size) {
+function calcDivHash(k, table_size) {
     return k % table_size;
 }
 
 function displayHT(table) {
-  displayHashTable(table);
+    displayHashTable(table);
 }
