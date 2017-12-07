8054cb30d26eb0ba7a9dcad3b1cc3166ff0bf9f3
Vainika <vainika.dara@gmail.com>
2017-12-07 14:05:28 -0500

    Multiple Element Display Initial Version

--- algocynfas.js
+++ algocynfas.js
@@ -23,9 +23,9 @@ const VERTICAL = 1;
 class DataElem {
 
   constructor(name = 'Data', val = null, shape = null) {
-    this.name = name;
-    this.val = val;
-    this.shape = shape;
+      this.name = name;
+      this.val = val;
+      this.shape = shape;
   }
 
   draw() {
@@ -36,54 +36,79 @@ class DataElem {
 class ListElem extends DataElem {
 
   constructor(val, shape = null) {
-    super('ListElem', val)
-    this.shape = new fabric.Rect({
-        fill: DEF_BG_COLOR,
-        width: DEF_ELEM_WIDTH,
-        height: DEF_ELEM_HEIGHT,
-        id: val,
-    });
-    this.dispText = new fabric.Text(String(val),
-            {fontSize: DEF_FONT, originX: CENTER, originY: CENTER});
-    this.group = new fabric.Group([ this.shape, this.dispText ],
-                    {left: 400, top: 40, angle: 0, id: val});
+      super('ListElem', val);
+      this.shape = this.setShape(val);
+      this.dispText = this.setText(val);
+      this.group = this.setGroup(this.shape, this.dispText);
   }
 
   draw(canvas, x, y) {
-      this.shape.originX = CENTER;
-      this.shape.originY = CENTER;
-      canvas.add(this.group)
+      canvas.add(this.setGroup(this.shape, this.dispText, x, y));
+  }
+
+  setShape(id) {
+      return new fabric.Rect({
+          fill: DEF_BG_COLOR,
+          originX: CENTER,
+          originY: CENTER,
+          width: DEF_ELEM_WIDTH,
+          height: DEF_ELEM_HEIGHT,
+          id: id,
+      });
+  }
+
+  setText(val) {
+      return new fabric.Text(String(val),
+              {fontSize: DEF_FONT, originX: CENTER, originY: CENTER});
+  }
+
+  setGroup(shape, dispText, x) {
+      console.log(x);
+      return new fabric.Group([ shape, dispText ],
+                      {left: 400, top: 40, angle: 0});
   }
 }
 
 class DataStructure extends DataElem {
+
   constructor() {
-    super('DataStructure')
-    this.dataElems = []
+      super('DataStructure');
+      this.dataElems = [];
   }
 
   add(elem) {
-    this.dataElems.push(elem)
+      this.dataElems.push(elem);
   }
+
+  setListPos(canvasWidth) {
+      return canvasWidth/2 - DEF_ELEM_WIDTH * Math.floor(this.dataElems.length/2);
+  }
+
+  setListElemPos(canvasWidth, elemIndex) {
+      return this.setListPos(canvasWidth) + (DEF_ELEM_WIDTH + 1) * elemIndex;
+  }
+
 }
 
 class List extends DataStructure {
+
   constructor(list = null) {
-    super();
-    this.setList(list);
+      super('List');
+      this.setList(list);
   }
 
   setList(list) {
       for (var valIndex in list) {
-          super.add(new ListElem(list[valIndex]))
+          super.add(new ListElem(list[valIndex]));
       }
   }
 
   draw(canvas, x, y) {
-    for (var elemIndex in this.dataElems) {
-        this.dataElems[elemIndex].draw(canvas, x, y)
-        x += DEF_ELEM_WIDTH
-    }
-    canvas.renderAll()
+      for (var elemIndex in this.dataElems) {
+          x = super.setListElemPos(canvas.width, elemIndex)
+          this.dataElems[elemIndex].draw(canvas, x, y);
+          //x += DEF_ELEM_WIDTH;
+      }
+      canvas.renderAll();
   }
 }
