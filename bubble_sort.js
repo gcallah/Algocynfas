ccc9a210431a6ea089a0e0c062b9f4305131c9a5
Vainika <vainika.dara@gmail.com>
2018-01-26 11:14:13 -0500

    Adding Bubble Sort.

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,12 +1,18 @@
-async function bubbleSort(elements) {
-    await displayList(elements);
-    for(var i = 0; i < elements.length ; i++) {
-        for(var j = elements.length - 1; j > i; j--) {
-            if(elements[j] < elements[j - 1]) {
-                await swapElem(elements, elements[j], elements[j - 1]);
-                await displayList(elements);
+async function bubbleSort(canvas, list) {
+    var myList = new List(canvas, list)
+    await myList.draw(true);
+    for(var i = 0; i < myList.size(); i++) {
+        for(var j = myList.size() - 1; j > i; j--) {
+            myList.highlightSwap(j);
+            await myList.draw();
+            if(myList.elemAt(j).getKey() < myList.elemAt(j - 1).getKey()) {
+                myList.highlight(j - 1);
+                await myList.draw();
+                myList.swap(j, j - 1)
+                await myList.draw();
             }
+            myList.unhighlight(i, j);
+            await myList.draw();
         }
     }
-    await displayList(elements);
 }
