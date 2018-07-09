cd001e233f515d6aa5397505a7c4a5aa61d261e3
Selina Zhang <yz4184@nyu.edu>
2018-07-09 01:04:45 -0400

    add comments and check errors

--- sort.js
+++ sort.js
@@ -104,73 +104,73 @@ async function quickSort(myList) {
     await mySort(myList, 0, myList.size() - 1);
 }
 
-async function mySort(myList, low, high) {
+async function mySort(myList, low, high) {                    // Refer to CLRS P 171
     if (!animeRunning) {
         return;
     }
 
-    if(low < high) {
-      var pIndex = await partition(myList, low, high);
+    if(low < high) {                                          // if (p < r)
+      var pIndex = await partition(myList, low, high);       // q= partition( A, p, r)
       myList.unhighlight(low, high);
       await myList.draw();
-      await mySort(myList, low, pIndex - 1);
-      await mySort(myList, pIndex + 1, high);
+      await mySort(myList, low, pIndex - 1);                 // QuickSort(A, p, q-1)
+      await mySort(myList, pIndex + 1, high);                // QuickSort(A, q+1, r)
     }
 
 }
 
-async function partition(myList, low, high) {
+async function partition(myList, low, high) {                //partition(A, p, r)
     if (!animeRunning) {
         return;
     }
 
-    var pivot = myList.elemAt(high).getKey();
+    var pivot = myList.elemAt(high).getKey();                // x = A[r]
     myList.highlightSwap(high);
     await myList.draw();
-    var i = (low - 1);
-    for(var j = low; j < high; j++) {
+    var i = (low - 1);                                       // i = p-1
+    for(var j = low; j < high; j++) {                        // for j = p to r - 1
         if (!animeRunning) {
             return;
         }
         myList.highlight(j);
         await myList.draw();
-        if(myList.elemAt(j).getKey() <= pivot) {
-            i++;
-            myList.swap(i, j);
+        if(myList.elemAt(j).getKey() <= pivot) {              // if A[j] <= x
+            i++;                                              // i = i+1
+            myList.swap(i, j);                                // exchange A[i] A[j]
             await myList.draw();
         }
     }
-    myList.swap(i + 1, high);
+    myList.swap(i + 1, high);                              // exchange A[j+1] with A[r]
     await myList.draw();
-    return i + 1;
+    return i + 1;                                          // return i+1
 }
 
 
 
 //  insertion sort below
 
-async function insertionSort(myList) {
+async function insertionSort(myList) {                // Refer to CLRS P18
     var j = 0;
     var i = 0;
 
     var delayTime = check_and_Delay();
     myList.setDelay(delayTime);
-    for (let dataElem of myList) {
+    for (let dataElem of myList) {                  
         if (!animeRunning) break;
-        var key = dataElem.getKey();
-        i = j - 1; // will be -1 first iteration!
+        var key = dataElem.getKey();                        // key = A[j];􏰀
+        i = j - 1; // will be -1 first iteration!           // Insert A[j] 􏰀 into the sorted sequence A[1----j-1] i=j-1
         myList.highlightSwap(j);
         await myList.draw();
-        while(i >= 0 && myList.elemAt(i).getKey() > key) {
+        while(i >= 0 && myList.elemAt(i).getKey() > key) {   //while i > 0 and A[i] > key
             if (!animeRunning) break;
             myList.highlight(i);
             await myList.draw();
-            myList.swap(i, i + 1)
-            i = i - 1;
+            myList.swap(i, i + 1)                           // A [i+1] = A [i]
+            i = i - 1;                                     // i = i -1
             await myList.draw();
         }
         await myList.draw();
-        myList.unhighlight(i + 1, j);
+        myList.unhighlight(i + 1, j);                    // A[i+1] = key
         await myList.draw();
         j++;
     }
@@ -178,23 +178,23 @@ async function insertionSort(myList) {
 
 // bubble sort below
 
-async function bubbleSort(myList) {
+async function bubbleSort(myList) {                      // Refer to CLRS P 40
 
     
     var delayTime = check_and_Delay();
     myList.setDelay(delayTime);
     
-    for(let i = 0; i < myList.size(); i++) {
-        for(let j = myList.size() - 1; j > i; j--) {
+    for(let i = 0; i < myList.size(); i++) {                    // for i = 0 to A.length -1 
+        for(let j = myList.size() - 1; j > i; j--) {               // for j= A.length downto i+1
             if (!animeRunning) break;
    
 
             myList.highlightSwap(j);
             await myList.draw();
-            if(myList.elemAt(j).getKey() < myList.elemAt(j - 1).getKey()) {
+            if(myList.elemAt(j).getKey() < myList.elemAt(j - 1).getKey()) {     // if A[j] < A [j-1]
                 myList.highlight(j - 1);
                 await myList.draw();
-                myList.swap(j, j - 1);
+                myList.swap(j, j - 1);                                        // Exchange A[j] A[j-1] 
                 await myList.draw();
             }
             myList.unhighlight(i, j);
