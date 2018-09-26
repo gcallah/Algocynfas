17f6a440a142b56f269e3fe7d1b9437e199be25a
Jing Song <ccs401@nyu.edu>
2018-09-25 20:43:47 -0400

    Initial commit of graphics.js | currently contains functions to create a canvas, circle, and rectangle.

---
+++ graphics.js
@@ -0,0 +1,28 @@
+function createCanvas(title='canvas') {
+    return (new fabric.Canvas(title))
+}
+
+function createCircle(x=500, y=500, radius=20)
+{
+    return new fabric.Circle(
+    {
+        radius: radius,
+        left: x,
+        top: y,
+        originX: 'center',
+        originY: 'center',
+        fill: 'white'
+    });
+}
+
+function createRectangle(x=500, y=500, height=20, width=20)
+{
+    return new fabric.Rect(
+    {
+        left: x,
+        top: y,
+        height: height,
+        width: width,
+        fill: 'white'
+    });
+}
