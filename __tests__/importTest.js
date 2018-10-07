50528dc826a812397e551bbd60791b9855567b84
Selina Zhang <yz4184@nyu.edu>
2018-10-07 00:30:07 -0400

    widget testing done

--- __tests__/importTest.js
+++ __tests__/importTest.js
@@ -1,7 +1,32 @@
-const common = require('../widgets.js');
+const widgets = require('../widgets.js');
 
 
 test("letter Num convert is fine", () => {
 
-  expect(common.func1(1)).toBe("b");
+	expect(widgets.func1(1)).toBe("b");
 });
+
+
+
+test("Correct legend is created", () => {
+
+	expect(widgets.func2("a","b",	"#008000", "#00FFFF")).toEqual({    
+		"data": [{
+			"title": "a",
+			"color": "#008000",
+		},
+		{
+			"title": "b",
+			"color": "#00FFFF",
+		}]
+	});
+
+});
+
+
+test("Make string input to list is fine", () => {
+	expect(widgets.func3("1,2  ,32,3,  4, 5",true)).toEqual(["1","2","32","3","4","5"]);
+	expect(widgets.func3("1,2,3,4,5")).toEqual(["1","2","3","4","5"]);
+});
+
+
--- widgets.js
+++ widgets.js
@@ -1,67 +1,88 @@
-//test jest
-function a(a, b) {
-    return a + b;
-}
 
-function getHTML(id) {
-  return document.getElementById(id);       // daisy
-}                                
-
+// logic layer (need to be tested)
 function splitInput(input,space = false){
   if (space){
-    return (input.split(" ").join("")).split(",");     //daisy
+    return (input.split(" ").join("")).split(",");     
   }
   return input.split(",");
 }
 
 
 function letterNumConvert(key){
-    if(Number.isInteger(key)){
-      var aAlph = {
-        0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h",
-        8: "i", 9: "j", 10: "k", 11: "l", 12: "m", 13: "n", 14: "o", 15: "p"}    // daisy
-        return aAlph[key];
+  if(Number.isInteger(key)){
+    var aAlph = {
+      0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h",
+      8: "i", 9: "j", 10: "k", 11: "l", 12: "m", 13: "n", 14: "o", 15: "p"}    
+      return aAlph[key];
 
-      }
+    }
     
     var aNum={
-        a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9, k: 10, 
-        l: 11, m: 12, n: 13,o: 14, p: 15
+      a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9, k: 10, 
+      l: 11, m: 12, n: 13,o: 14, p: 15
     }
     return aNum[key];
-}
+  }
 
-function check_and_Delay(){        //daisy
+
+  function create_legend(title1, title2 , c1, c2) {   
+    const legend = {    
+      "data": [{
+        "title": title1,
+        "color": c1,
+      },
+      {
+        "title": title2,
+        "color": c2,
+      },]
+    };
+    return legend;
+  }
+
+  function stopAnime() {               
+    animeRunning = false;
+  }
+
+
+
+// display layer
+
+
+  function getHTML(id) {
+    return document.getElementById(id);       
+  }                                
+
+  function check_and_Delay(){       
     if(getHTML('Fast').checked)        
     {
-       return 300;   
-    }
-    else if(getHTML('Medium').checked)
-    {
-       return 800;
-    }
-    else
-    {
-       return 2000;
-    }
-    
-}
-
-function noticeErr(message,id="") {         //Yujia
-  if(id!=""){
-      getHTML(id).style.background="red";
+     return 300;   
    }
-   alert(message);
+   else if(getHTML('Medium').checked)
+   {
+     return 800;
+   }
+   else
+   {
+     return 2000;
+   }
+
+ }
+
+ function noticeErr(message,id="") {         
+  if(id!=""){
+    getHTML(id).style.background="red";
+  }
+  alert(message);
 }
 
 function correctErr(id) {
-    getHTML(id).style.background="#FCF5DB";
+  getHTML(id).style.background="#FCF5DB";
 }
 
 
-function drawListOnScreen(currentNumbersList) {                  //sort.js and hashTable.js
-    canvas.clear();
-    fList = window.createList(canvas, currentNumbersList);
+function drawListOnScreen(currentNumbersList) {                 
+  canvas.clear();
+  fList = window.createList(canvas, currentNumbersList);
 }
 
 
@@ -71,88 +92,87 @@ function graphRefresh(){
 
 }
 
-function setWeight(){                 // Yujia
-   var choice = getHTML("graphWeight").selectedIndex;
-   var wBox = getHTML("weights");
-    if(choice == 0){
-      weight = 0;
-      wBox.style.display = "none";
-    }
-    else{
-      weight = 1;
-      wBox.style.display = "inline";
-    }
-
+function setWeight(){                 
+ var choice = getHTML("graphWeight").selectedIndex;
+ var wBox = getHTML("weights");
+ if(choice == 0){
+  weight = 0;
+  wBox.style.display = "none";
 }
-function setDir(){                                 //Yujia
-    var choice = getHTML("graphType").selectedIndex;
-    if(choice == 0){
-      dir = 0;
-    }
-    else{
-      dir = 1;
-    }
+else{
+  weight = 1;
+  wBox.style.display = "inline";
 }
 
-
-
-
-
-function stopAnime() {               // Yujia
-    animeRunning = false;
-    }
-
-
-
-
-function create_legend(title1, title2 , c1, c2) {   // Daisy
-    const legend = {    
-        "data": [{
-                    "title": title1,
-                    "color": c1,
-                },
-                {
-                    "title": title2,
-                    "color": c2,
-                        },]
-    };
-    return legend;
 }
+function setDir(){                                
+  var choice = getHTML("graphType").selectedIndex;
+  if(choice == 0){
+    dir = 0;
+  }
+  else{
+    dir = 1;
+  }
+}
+
+
 
 function display_legend(id,title1, title2, c1= '#ffb380', c2 = '#99ccff') {
   var legend = create_legend(title1, title2, c1, c2);
   return getHTML(id).innerHTML =
   `<div class='list-group col-3'>
-    <div class='list-group-item' style = background-color:${legend.data[0].color}>
-    ${legend.data[0].title}
-    </div>
-    <div class='list-group-item' style = background-color:${legend.data[1].color}>
-    ${legend.data[1].title}
-    </div>
+  <div class='list-group-item' style = background-color:${legend.data[0].color}>
+  ${legend.data[0].title}
+  </div>
+  <div class='list-group-item' style = background-color:${legend.data[1].color}>
+  ${legend.data[1].title}
+  </div>
   </div>`;
 }
 
 
-
+// testing part (logic layer)
 
 module.exports = {
   func1: function letterNumConvert(key){
     if(Number.isInteger(key)){
       var aAlph = {
         0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h",
-        8: "i", 9: "j", 10: "k", 11: "l", 12: "m", 13: "n", 14: "o", 15: "p"}    // daisy
+        8: "i", 9: "j", 10: "k", 11: "l", 12: "m", 13: "n", 14: "o", 15: "p"}    
         return aAlph[key];
 
       }
-    
-    var aNum={
+
+      var aNum={
         a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9, k: 10, 
         l: 11, m: 12, n: 13,o: 14, p: 15
-    }
-    return aNum[key];
-},
+      }
+      return aNum[key];
+    },
 
-};
+    func2: function create_legend(title1, title2 , c1, c2) {   
+      const legend = {    
+        "data": [{
+          "title": title1,
+          "color": c1,
+        },
+        {
+          "title": title2,
+          "color": c2,
+        },]
+      };
+      return legend;
+    },
+
+    func3: function splitInput(input,space = false){
+      if (space){
+        return (input.split(" ").join("")).split(",");    
+      }
+      return input.split(",");
+    },
+
+
+  };
 
 
 
