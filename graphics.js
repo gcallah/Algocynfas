502d0001cb10a5fa8bd598dd4b232eb92e3d4b77
Jing Song <ccs401@nyu.edu>
2018-10-20 22:26:21 -0400

    Changed basic shapes to classes with parent class Shape() that has the function rotate. Groups and texts are broken as of now due to this update

--- graphics.js
+++ graphics.js
@@ -1,9 +1,10 @@
-function createCanvas(title = 'canvas') {
-  return new fabric.Canvas(title);
+function Canvas(title = 'canvas') {
+    this.fabricObject = new fabric.Canvas(title);
 }
 
-function createCircle(x = 50, y = 50, radius = 50, color = 'red') {
-  return new fabric.Circle({
+function Circle(x = 50, y = 50, radius = 50, color = 'red') {
+    Shape.call(this);
+    this.object = new fabric.Circle({
     radius: radius,
     left: x,
     top: y,
@@ -13,8 +14,45 @@ function createCircle(x = 50, y = 50, radius = 50, color = 'red') {
   });
 }
 
-function createRectangle(x = 50, y = 50, height = 50, width = 50, color='blue') {
-  return new fabric.Rect({
+function Rectangle(x = 50, y = 50, height = 50, width = 50, color='blue') {
+    Shape.call(this);
+    this.object = new fabric.Rect({
+    left: x,
+    top: y,
+    height: height,
+    width: width,
+    fill: color,
+    originX: 'originX',
+    originY: 'originY',
+  });
+}
+
+function Shape(object) {
+    this.object = object;
+    this.rotate = function(angle) {
+        this.object.rotate(angle)
+    };
+}
+
+function Group(shapeArray, left, top) {
+    this.object = new fabric.Group(shapeArray, {
+    left: x,
+    top: y,
+  });
+}
+
+
+function SolidArrow(x = 50, y = 50, height = 50, length = 100, color = 'black') {
+    Shape.call(this);
+    var rectangle = new Rectangle(x, y, height / 2, length, color);
+    var triangle = new Triangle(x / 2 + length, y, 50, height, color);
+    triangle.rotate(90);
+    this.object = new Group([rectangle, triangle], x, y)
+}
+
+function Triangle(x = 50, y = 50, width = 50, height = 50, color = 'yellow') {
+    Shape.call(this);
+    this.object = new fabric.Triangle({
     left: x,
     top: y,
     height: height,
@@ -29,11 +67,6 @@ function createSolidArrow(x = 50, y = 50, height = 50, length = 100, color = 'bl
     var rectangle = createRectangle(x, y, height / 2, length, color);
     var triangle = createTriangle(x / 2 + length, y, 50, height, color);
     rotate(triangle, 90);
-    var group = new fabric.Group([rectangle, triangle], {
-        left: x,
-        top: y
-    });
-    return group;
 }
 
 function createText(text, x = 50, y = 50, color = 'black', fontSize = 30) {
@@ -67,35 +100,41 @@ function createTextRectangle(text, x=50, y=50, height=50, width=50, textColor = 
   return group;
 }
 
-function createTriangle(x = 50, y = 50, width = 50, height = 50, color = 'red') {
-    return new fabric.Triangle({
-    left: x,
-    top: y,
-    height: height,
-    width: width,
-    fill: color,
-    originX: 'originX',
-    originY: 'originY',
-  });
-}
-
-function rotate(fabricObject, angle = 15) {
-    fabricObject.rotate(angle)
-}
-
 function create() {
-  var canvas = createCanvas();
-
-  //var circle = createCircle(50, 50, 50, 'white');
-  //canvas.add(circle);
+  var canvas = new Canvas().fabricObject;
+ 
+  //Testing parent class
+  var shape = new Shape(new fabric.Circle({
+    radius: 10,
+    left: 20,
+    top: 20,
+    originX: 'center',
+    originY: 'center',
+    fill: 'white',
+  })).object;
+  canvas.add(shape);
+    
+  var circle = new Circle(x = 100).object;
+  canvas.add(circle);
+    
+  var rectangle = new Rectangle(x = 200, y = 100).object;
+  rectangle.rotate(30);
+  canvas.add(rectangle);
+  
+  var triangle = new Triangle(x = 300, y = 75).object;
+  triangle.rotate(100);
+  canvas.add(triangle);
+    
+  //ar arrow = new SolidArrow().object;
+    
   //var text = createText('text', 50, 50, 'black', 50/2);
   //canvas.add(text);
   //var textCircle = createTextCircle('textCircle', 200, 50);
   //canvas.add(textCircle);
-  var textRectangle = createTextRectangle('textRect', 300, 50);
-  canvas.add(textRectangle);
+  //var textRectangle = createTextRectangle('textRect', 300, 50);
+  //canvas.add(textRectangle);
 
-  var arrow = createSolidArrow();
-  canvas.add(arrow);
-  rotate(textRectangle, 90);
+  //
+  //canvas.add(arrow);
+  //rotate(textRectangle, 90);
 }
