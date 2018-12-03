340e97b0c55364161e7585ed510ea62055e75531
dl3474 <43046902+dl3474@users.noreply.github.com>
2018-12-02 22:04:19 -0500

    started TestAlertMST

--- seleniumTests/testAlerts.py
+++ seleniumTests/testAlerts.py
@@ -4,7 +4,8 @@ from unittest import TestCase, main
 
 from random import randint
 
-path = #change path to path of chromedriver.exe
+#path = #change path to path of chromedriver.exe
+path = r"C:\Users\dli19\Desktop\chromedriver\lib\chromedriver\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=path)
 
 #Parent Class - General TestAlert
@@ -22,47 +23,50 @@ class TestAlert(TestCase):
     def getById(self, _id):
         return driver.find_element_by_id(_id)
 
+    def randNum(self):
+        return randint(0, 100)
+    
 #Child Class - TestAlert for Sort
 class TestAlertSort(TestAlert):
     
     def __init__(self):
-        self.validInput = self.validOrInvalid()
+        self.validInput = self.valid()
         
-    def validOrInvalid(self):
-        if (randint(0, 100) > 50):
+    def valid(self):
+        if (self.randNum() > 50):
             return False 
         return True
     
     def generateValidInput(self):
         #valid input = any list of nums (-99 < num < 99)
-        keys = "";
-        validInput = randint(0,99);
+        keys = ""
+        validInput = randint(0,99)
         for i in range(1,6):
-            keys += str(validInput) + ",";
-            validInput = randint(0,99);
+            keys += str(validInput) + ","
+            validInput = randint(0,99)
             if (validInput % i == 1): #randomize negative inputs
-                validInput *= -1;
-        keys += str(validInput);
-        return keys;
+                validInput *= -1
+        keys += str(validInput)
+        return keys
     
     def generateInvalidInput(self):
         #invalidInput = any non-integers || input.length < 2 
-        charNum = randint(0, 126);
+        charNum = randint(0, 126)
         if (charNum <= 32): #input.length = 0
-            return "";
+            return ""
         else:
             if (charNum % 2 == 0): #input.length = 1
-                return chr(charNum);
+                return chr(charNum)
             else: #input.length > 1
                 return (self.generateValidInput() + "," + chr(charNum))
     
     def enterInputs(self, inputBox):
         _input = ""
         if (self.validInput):
-            _input = self.generateValidInput();
+            _input = self.generateValidInput()
         else:
-            _input = self.generateInvalidInput();
-        inputBox.send_keys(_input);
+            _input = self.generateInvalidInput()
+        inputBox.send_keys(_input)
         print(_input)
     
     def testAlert(self):
@@ -97,7 +101,7 @@ class TestAlertHeap(TestAlert):
             return invalidInput
     
     def setSize(self):
-        #setSizeDefaultValue = 10
+        #defaultValue = 10
         self.getById("set-button").click()
         
     def chooseFunc(self, validInput):
@@ -118,20 +122,20 @@ class TestAlertHeap(TestAlert):
         self.getLink("Hash table").click()
         inputID = "tableSize"
         
-        choices = randint(0, 100) 
-        if (choices < 33): #test Set Size with invalid input
+        choices = self.randNum()
+        if (choices <= 33): #test Set Size with invalid input
             inputBox = self.getById(inputID)
             inputBox.clear()
             #invalidInput = < 2 || > 20
             inputBox.send_keys(self.generateInvalidInput(2, 20))
             self.setSize()
             
-        elif (33 < choices < 66): #test chooseFunc with invalid function
+        elif (33 < choices <= 66): #test chooseFunc with invalid function
             inputID = "funcChoices"
-            validFunc = True
+            validFunc = False
             #need to set a valid size to test invalid function
             self.setSize()
-            self.chooseFunc(False)
+            self.chooseFunc(validFunc)
             self.getById("run-button").click()
             
         else: #test Hash with invalid input
@@ -161,7 +165,42 @@ class TestAlertHeap(TestAlert):
                 print("Error: Function Choices: invalid input is given and alert doesn\'t pop up")
             else:
                 print("Error: Hash: invalid input is given and alert doesn\'t pop up")
+        
         print()
+
+#Child Class - TestAlert for Minimum Spanning Tree
+class TestAlertMST(TestAlert):
+    
+    def addNodes(self, validInput):
+        inputBox = self.getById("nodeNum")
+        lowestValidInput = 1 #2?
+        highestValidInput = 16
+        if validInput:
+            inputBox.send_keys(randint(lowestValidInput, highestValidInput))
+        else:
+            invalidInput = randint(highestValidInput * -1, lowestValidInput - 1)
+            inputBox.send_keys(invalidInput)
+            print(invalidInput)
+        self.getById("add-node-button").click()
+        
+    def testAlert(self):
+        print("TestAlertMST")
+        self.loadPage()
+        self.getLink("Find a Minimum Spanning Tree").click()
+        inputID = "nodeNum"
+        
+        choices = 1 #self.randNum()
+        if (choices <= 33): #test Create Nodes with invalid input
+            validNodeNum = False
+            self.addNodes(validNodeNum)
+            
+        try:
+            driver.switch_to.alert.accept()  
+            if inputID == "nodeNum":
+                print("Ran as expected: Create Nodes: Invalid input is given and alert pops up")
+        except:
+            if inputID == "nodeNum":
+                print("Error: Create Node: invalid input is given and alert doesn\'t pop up")
         
 if __name__ == "__main__": 
     
@@ -174,6 +213,9 @@ if __name__ == "__main__":
     heap = TestAlertHeap()
     heap.testAlert()
     
-    testAlert.closePage()
+    mst = TestAlertMST()
+    mst.testAlert()
+    
+    #testAlert.closePage()
     
 
